c337148a72de0b7c110bbea0b7fbb18834debffa
John Mark Schofield <jms@schof.org>
2014-04-01 20:05:04 -0700

    Some debug code

--- master.py
+++ master.py
@@ -6,9 +6,11 @@ import commands
 
 
 def execute(command):
-    status, output = commands.getstatsoutput(command)
+    status, output = commands.getstatusoutput(command)
     if status != 0:
         print('ERROR: Command %s gave status %s and output %s' %(command, status, output))
+    else:
+        print('INFO: This worked.')
 
 while True:
     execute("/app/stockreport.py")
