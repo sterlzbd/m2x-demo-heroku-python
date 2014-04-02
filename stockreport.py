c2e4ba7ca245e7d51dd42dc2fceb67edf3472c99
John Mark Schofield <jms@schof.org>
2014-04-01 18:32:31 -0700

    Reinitializing git repo to see if I can get Heroku to not hate me.

---
+++ stockreport.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import time
+import datetime
+
+import ystockquote
+
+from m2x.client import M2XClient
+
+TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
+print("%s: Starting stockreport.py run" % time.strftime(TIMEFORMAT))
+
+BLUEPRINT_NAME = "stockreport-vagrant"
+
+
+# Load config
+APIKEY = open('/vagrant/m2x_api_key.txt').read().strip()
+now = datetime.datetime.now()
+ATT_Stock_Price = ystockquote.get_price('T')
+
+# Now let's create a blueprint:
+client = M2XClient(key=APIKEY)
+stockreport_blueprint_exists = False
+for blueprint in client.blueprints:
+    if blueprint.name == BLUEPRINT_NAME:
+        stockreport_blueprint_exists = True
+        break
+
+if not stockreport_blueprint_exists:
+    blueprint = client.blueprints.create(
+        name=BLUEPRINT_NAME,
+        description="Stockreport Example Blueprint",
+        visibility="private")
+
+# We get the feed that was automatically created when we created the blueprint
+feed = blueprint.feed
+
+# Now we need to get the stream for AT&T's stock ticker symbol ("T")
+ATT_Stream_Exists = False
+for stream in feed.streams:
+    if stream.name.upper() == "T":
+        ATT_Stream_Exists = True
+        break
+
+if not ATT_Stream_Exists:
+    stream = feed.streams.create('T')
+
+stream.update(unit={'label': 'Dollars', 'symbol': '$'})
+
+stream.values.add_value(ATT_Stock_Price, now)
+
+print("%s: Ending stockreport.py run" % time.strftime(TIMEFORMAT))
+print
