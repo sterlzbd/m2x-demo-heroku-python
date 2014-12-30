9694b9bb6568cab7d4cbdf1d01524b38c2650ec8
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2014-12-30 22:37:47 +0800

    Update stockreport to v2

--- stockreport.py
+++ stockreport.py
@@ -24,39 +24,36 @@ sys.stderr = Unbuffered(sys.stderr)
 
 
 print("Starting stockreport.py run")
-BLUEPRINT_NAME = "stockreport-heroku"
+DEVICE_NAME = "stockreport-heroku"
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 
 APIKEY = open(os.path.join(WORKING_DIR, 'm2x_api_key.txt')).read().strip()
 NOW = datetime.datetime.now()
 ATT_STOCK_PRICE = ystockquote.get_price('T')
 
-# Now let's create a blueprint:
+# Now let's create a device:
 client = M2XClient(key=APIKEY)
-stockreport_blueprint_exists = False
-for blueprint in client.blueprints:
-    if blueprint.name == BLUEPRINT_NAME:
-        stockreport_blueprint_exists = True
+stockreport_device_exists = False
+for device in client.devices:
+    if device.name == DEVICE_NAME:
+        stockreport_device_exists = True
         break
 
-if not stockreport_blueprint_exists:
-    blueprint = client.blueprints.create(
-        name=BLUEPRINT_NAME,
-        description="Stockreport Example Blueprint",
+if not stockreport_device_exists:
+    device = client.devices.create(
+        name=DEVICE_NAME,
+        description="Stockreport Example Device",
         visibility="private")
-
-# We get the feed that was automatically created when we created the blueprint
-feed = blueprint.feed
 
 # Now we need to get the stream for AT&T's stock ticker symbol ("T")
 ATT_Stream_Exists = False
-for stream in feed.streams:
+for stream in device.streams:
     if stream.name.upper() == "T":
         ATT_Stream_Exists = True
         break
 
 if not ATT_Stream_Exists:
-    stream = feed.streams.create('T')
+    stream = device.streams.create('T')
 
 stream.update(unit={'label': 'Dollars', 'symbol': '$'})
 
