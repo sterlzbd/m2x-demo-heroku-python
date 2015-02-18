06587933b1cd4be057024d5fc2cba32e8304df70
Sterling Brim-DeForest <s.brimdeforest@gmail.com>
2015-02-17 17:25:59 -0800

    updates to improve encapsulation

--- master.py
+++ master.py
@@ -1,39 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import os
 import sys
 import time
-import os.path
-import subprocess
-
-WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
-
-# This is necessary to get unbuffered output. We want unbuffered output so each line we print gets a
-# timestamp from Heroku at the correct time.
-class Unbuffered(object):
-   def __init__(self, stream):
-       self.stream = stream
-   def write(self, data):
-       self.stream.write(data)
-       self.stream.flush()
-   def __getattr__(self, attr):
-       return getattr(self.stream, attr)
-
-sys.stdout = Unbuffered(sys.stdout)
-sys.stderr = Unbuffered(sys.stderr)
+from stockreport import post_stock_price
 
 
-def execute(command):
-    p = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE)
-    while True:
-        out = p.stderr.read(1)
-        if out == '' and p.poll() != None:
-            break
-        if out != '':
-            sys.stdout.write(out)
-            sys.stdout.flush()
+DEVICE_NAME = os.environ.get('DEVICE_NAME', 'stockreport-heroku')
+
+APIKEY = os.environ['MASTER_API_KEY']
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
+
 
 while True:
-    execute(os.path.join(WORKING_DIR, "stockreport.py"))
+    print "Posting stock prices."
+    # Post stock price of AT&T
+    post_stock_price("T", APIKEY, DEVICE_NAME)
+    print "Stock prices posted."
     time.sleep(60)
 
--- stockreport.py
+++ stockreport.py
@@ -1,62 +1,34 @@
 #!/usr/bin/env python
 
-import sys
-import time
-import os.path
-import datetime
+from requests.exceptions import HTTPError
+from datetime import datetime
 
 import ystockquote
 from m2x.client import M2XClient
 
-# This is necessary to get unbuffered output. We want unbuffered output so each line we print gets a
-# timestamp from Heroku at the correct time. Otherwise all will have the same timestamp when the program exits.
-class Unbuffered(object):
-   def __init__(self, stream):
-       self.stream = stream
-   def write(self, data):
-       self.stream.write(data)
-       self.stream.flush()
-   def __getattr__(self, attr):
-       return getattr(self.stream, attr)
 
-sys.stdout = Unbuffered(sys.stdout)
-sys.stderr = Unbuffered(sys.stderr)
+def post_stock_price(symbol, apikey, devicename):
+    '''
+    Retrieve the stock price for the given ticker symbol ("T" for AT&T) and
+    post it in the correct M2X data stream.
+    '''
+    client = M2XClient(key=apikey)
 
+    # Find the correct device if it exists, if not create it.
+    try:
+        device = [d for d in client.devices(q=devicename) if d.name == devicename][0]
+    except IndexError:
+        device = client.create_device(name=devicename,
+                                      description="Stockreport Example Device",
+                                      visibility="private")
 
-print("Starting stockreport.py run")
-DEVICE_NAME = "stockreport-heroku"
-WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
+    # Get the stream if it exists, if not create the stream.
+    try:
+        stream = device.stream(symbol)
+    except HTTPError:
+        stream = device.create_stream(symbol)
+        device.update_stream(symbol, unit={'label': 'Dollars', 'symbol': '$'})
 
-APIKEY = open(os.path.join(WORKING_DIR, 'm2x_api_key.txt')).read().strip()
-NOW = datetime.datetime.now()
-ATT_STOCK_PRICE = ystockquote.get_price('T')
-
-# Now let's create a device:
-client = M2XClient(key=APIKEY)
-stockreport_device_exists = False
-for device in client.devices:
-    if device.name == DEVICE_NAME:
-        stockreport_device_exists = True
-        break
-
-if not stockreport_device_exists:
-    device = client.devices.create(
-        name=DEVICE_NAME,
-        description="Stockreport Example Device",
-        visibility="private")
-
-# Now we need to get the stream for AT&T's stock ticker symbol ("T")
-ATT_Stream_Exists = False
-for stream in device.streams:
-    if stream.name.upper() == "T":
-        ATT_Stream_Exists = True
-        break
-
-if not ATT_Stream_Exists:
-    stream = device.streams.create('T')
-
-stream.update(unit={'label': 'Dollars', 'symbol': '$'})
-
-stream.values.add_value(ATT_STOCK_PRICE, NOW)
-
-print("Ending stockreport.py run")
+    postime = datetime.now()
+    stock_price = ystockquote.get_price(symbol).encode('utf-8')
+    stream.add_value(stock_price, postime)
